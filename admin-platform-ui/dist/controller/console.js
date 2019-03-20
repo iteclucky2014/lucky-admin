7faeaac45cd01b0a85e3b7a66ddb6f3b89087315
iteclucky2014 <fanqietou@126.com>
2019-03-20 16:43:21 +0800

    commit by lucky 2019/03/20

--- admin-platform-ui/dist/controller/console.js
+++
@@ -1,2 +0,0 @@
-/** layuiAdmin.pro-v1.1.0 LPPL License By http://www.layui.com/admin/ */
- ;layui.define(function(i){layui.use(["admin","carousel"],function(){layui.$,layui.admin,layui.carousel,layui.element,layui.device()}),i("console",{})});
